66deeaf4e062a78d65a05d1d4f4bcecbde220287
Pablo Periañez <pabpercab1@alum.us.es>
2024-10-05 10:53:22 +0100

    feat: Basic structure

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,53 @@
+import React from 'react';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 function Home() {
     return (
-        <Container>
+        <Container sx={{ mt: 12 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Grid>
+                    <Typography variant="h6" component="div" gutterBottom>
+                        Objects
+                    </Typography>
+                    <List>
+                        <ListItem>
+                            <ListItemText primary="Item 1" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Item 2" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Item 3" />
+                        </ListItem>
+                    </List>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="h6" component="div" gutterBottom>
+                            Asteroids
+                        </Typography>
+                        <List>
+                            <ListItem>
+                                <ListItemText primary="Item 1" />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Item 2" />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText primary="Item 3" />
+                            </ListItem>
+                        </List>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img src="test.png" alt="Description" style={{ width: '100%' }} />
+                </Grid>
+            </Grid>
         </Container>
     );
 }
