95e73777d0754a1d39ede9c365329a35c9071ae5
Pablo Periañez <pabpercab1@alum.us.es>
2024-10-05 14:54:21 +0100

    fix: icon detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardContent, Typography, Button, Avatar } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachIcon from '@mui/icons-material/BeachAccess';
 
 const Detail = ({ item, onBack }) => {
+    const getIcon = (iconName) => {
+        switch (iconName) {
+            case 'image':
+                return <ImageIcon />;
+            case 'work':
+                return <WorkIcon />;
+            case 'beach':
+                return <BeachIcon />;
+            default:
+                return null;
+        }
+    };
+
     return (
         <Card>
             <CardContent>
                 <Button variant="contained" color="primary" onClick={onBack} sx={{ mb: 2 }}>
                     <ArrowBackIcon />
                 </Button>
+                <Avatar sx={{ mb: 2 }}>
+                    {getIcon(item.icon)}
+                </Avatar>
                 <Typography variant="h5" component="div">
                     {item.primary}
                 </Typography>
