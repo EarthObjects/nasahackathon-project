a7444968db05dd8b0996955f7943d284188570d7
Pablo Periañez <pabpercab1@alum.us.es>
2024-10-05 10:40:59 +0100

    feat: Added navbar

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function Home() {
+    return (
+        <Container>
+        </Container>
+    );
+}
+
+export default Home;
