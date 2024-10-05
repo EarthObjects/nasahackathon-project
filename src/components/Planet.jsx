524483c29a76debd2ece6ecb460292e423e7b486
mxschll <max@schoell.co>
2024-10-05 11:02:48 +0100

    add planet component

---
+++ src/components/Planet.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+
+function Planet({ name }) {
+    const mesh = useRef();
+    const texture = new TextureLoader().load(`/textures/${name.toLowerCase()}.jpg`);
+
+    useFrame(() => {
+        mesh.current.rotation.y += 0.001;
+    });
+
+    return (
+        <mesh ref={mesh}>
+            <sphereGeometry args={[1, 32, 32]} />
+            <meshStandardMaterial map={texture} />
+        </mesh>
+    );
+}
+
+export default Planet;
